1760afa47bc837290eace106751734d27e7e7739
Adhiraaj28 <2022wb86325@wilp.bits-pilani.ac.in>
2026-09-25 00:41:15 +0530

    Update source code for CI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from BITS DevOps Lab - 2022wb86325");
+        System.out.println("Hello from BITS DevOps Lab - 2022wb86325- Updated");
 
     }
 
