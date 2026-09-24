90806c0eabd9236158e5662a66cf5a0ec3420787
Adhiraaj28 <2022wb86325@wilp.bits-pilani.ac.in>
2026-09-24 22:58:52 +0530

    Initial Maven Project for DevOPS Lab

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from BITS DevOps Lab - 2022wb86325");
+
+    }
+
+}
+
